e7fa7fb0444d0bbfb8cf29dff0dfee83b2bdad4c
Troy <troykirin@gmail.com>
2020-02-04 13:44:10 -0800

    create saveTranscription to txt file func.

--- playground/MVP.py
+++ playground/MVP.py
@@ -27,30 +27,36 @@ transcription = r.recognize_google(audio)
 print(transcription)
 
 # %%
-# save to a text file
-file = open(
-    r"/Users/troy/APFM/ROOT_CO/Snippets/transcriptions/latest.txt", "w+")
-test = 'test'
-file.write(test)
-file.close()
+# Function to save transcription string to a text file at x directory
 
-# %% analyze audio in snippets
-test = sr.AudioFile(
-    "python-speech-recognition/audio_files/test.wav")
+
+def saveTranscriptionToFile(transcription):
+    file = open(
+        r"/Users/troy/APFM/ROOT_CO/Snippets/transcriptions/latest.txt", "w+")
+    file.write(transcription)
+    file.close()
+
+
+# %% Analyze audio in snippets
+test = processAudioFile  # reuse audiofile from before
 
 with test as source:
-    audio1 = r.record(source, duration=40)
-    audio2 = r.record(source, duration=40)
+    audio1 = r.record(source, duration=30)
+    # audio2 = r.record(source, duration=40)
     # audio3 = r.record(source, duration=40)
     # audio4 = r.record(source, duration=40)
     # audio5 = r.record(source, duration=40)
     # audio6 = r.record(source, duration=40)
     # audio7 = r.record(source, duration=40)
 
-type(audio)
-# two diff audio
-print(r.recognize_google(audio1))
-print(r.recognize_google(audio2))
+# %% Save snippets to File
+print(type(audio1))
+
+# Run transcription in Cloud
+resultFromCloud = r.recognize_google(audio1)
+saveTranscriptionToFile(resultFromCloud)
+print(resultFromCloud)
+# print(r.recognize_google(audio2))
 # print(r.recognize_google(audio3))
 # print(r.recognize_google(audio4))
 # print(r.recognize_google(audio5))
