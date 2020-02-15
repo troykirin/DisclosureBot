500ce27faab5d374da90a79504f53e01d1fbc88b
Troy <troykirin@gmail.com>
2020-02-15 10:57:46 -0800

    print statements fix

--- gcp_speech2text/rubric_creation_tool.py
+++ gcp_speech2text/rubric_creation_tool.py
@@ -27,16 +27,21 @@ print(path)
 os.chdir(path_transcribe)
 with open('./rubric_names.txt') as f:
     reader = csv.reader(f, delimiter='\n')
+    count = 0
     for row in reader:
         print(f"Filename: {row[0]}")  # print row, access first element
+        count += 1
+        print(f"\tThis is file #{count}")
 
+    print(f"Total file count: {count}")
 
 # %% Duplication step
 os.chdir(path_CallRubrics)
 
 
 # %%
-shutil.copy('_Template_Colorado Call Disclosure Rubric.xlsx', 'test.xlsx')
+for
+shutil.copy('_Template_Colorado Call Disclosure Rubric.xlsx', 'test.xls')
 
 
 # %%
