006221d14a83d727453e66f5b91ebe2131986484
Troy <troykirin@gmail.com>
2020-02-15 10:48:25 -0800

    duplicate template test

--- gcp_speech2text/rubric_creation_tool.py
+++ gcp_speech2text/rubric_creation_tool.py
@@ -12,7 +12,9 @@ print(path_transcribe)
 
 # %%
 # Change directory to call rubrics to save at
-os.chdir("/Users/troy/APFM/ROOT_CO/Call Rubrics")
+path_CallRubrics = "/Users/troy/APFM/ROOT_CO/Call Rubrics"
+
+os.chdir(path_CallRubrics)
 
 # %%
 # Check
@@ -27,6 +29,14 @@ with open('./rubric_names.txt') as f:
     reader = csv.reader(f, delimiter='\n')
     for row in reader:
         print(f"Filename: {row[0]}")  # print row, access first element
+
+
+# %% Duplication step
+os.chdir(path_CallRubrics)
+
+
+# %%
+shutil.copy('_Template_Colorado Call Disclosure Rubric.xlsx', 'test.xlsx')
 
 
 # %%
