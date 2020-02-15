7363a111610d3996638ef7e3d2d5e8aa8abc43b8
Troy <troykirin@gmail.com>
2020-02-15 11:15:40 -0800

    finished

--- gcp_speech2text/rubric_creation_tool.py
+++ gcp_speech2text/rubric_creation_tool.py
@@ -23,25 +23,35 @@ print(path)
 
 
 # %% Open file
-
 os.chdir(path_transcribe)
+
+# get number of files to create
+numOfFiles = 0
+fileList = []
+
 with open('./rubric_names.txt') as f:
     reader = csv.reader(f, delimiter='\n')
     count = 0
     for row in reader:
         print(f"Filename: {row[0]}")  # print row, access first element
+        fileList.append(row[0])
         count += 1
         print(f"\tThis is file #{count}")
 
     print(f"Total file count: {count}")
+    numOfFiles = count
 
-# %% Duplication step
+
+# %%
+print(fileList[0])
+
+# %%
 os.chdir(path_CallRubrics)
 
+for element in fileList:
+    # get filename
+    filename = element + ".xlsx"
+    # creation
+    shutil.copy('_Template_Colorado Call Disclosure Rubric.xlsx', filename)
 
-# %%
-for
-shutil.copy('_Template_Colorado Call Disclosure Rubric.xlsx', 'test.xls')
-
-
-# %%
+    # %%
