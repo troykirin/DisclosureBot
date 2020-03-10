8eab88129caa7d354b0c6975a6b1853349bf1333
Troy <troykirin@gmail.com>
2020-03-09 19:40:15 -0700

    psudeocode of bot chat grading

---
+++ botGradeChat.py
@@ -0,0 +1,9 @@
+# --- CHAT Grading with bot ---
+
+# check if header exists
+
+# save var of lead name
+# compare against entire dialog if lead name shows up
+# will need a bool --- t/f for first name and last name
+
+# if name does not show up, return grade fail
